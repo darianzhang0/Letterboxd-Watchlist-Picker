627ad232dfa24d3f7d71fac3824025d87b958f76
darianzhang0 <77823043+darianzhang0@users.noreply.github.com>
2023-05-09 00:47:46 -0400

    Update htmlcreator.py

--- htmlcreator.py
+++ htmlcreator.py
@@ -22,19 +22,33 @@ def HTMLCreator(movieList, args):
             key = random.choice(list(movieList))
             file.write(f'<h4>Random Movie Choice from Watchlist: <a href={movieList[key]}>{key} </h4>')
 
-        file.write('<table border="1" width="100%"><tr><th>Movie</th><th>Director</th><th>Runtime</th><th>Genre(s)</th></tr>\n')
+        file.write('<table border="1" width="100%"><tr><th>Movie</th><th>Director(s)</th><th>Runtime</th><th>Genre(s)</th></tr>\n')
         for key in movieList:
-            strRW = "<tr><td>" + f"<a href={movieList[key]}>{key}</a>"
+            tableRow = "<tr><td>" + f"<a href={movieList[key]}>{key}</a>"
 
             userWatchlistUrl = f'{movieList[key]}'
             html_text = requests.get(userWatchlistUrl).text
             soup = BeautifulSoup(html_text, 'html.parser')
-            director = soup.find('span', class_ = "prettify")
-            length = soup.find('p', class_ = "text-link text-footer")
-            genre = soup.find('div', class_ = "text-sluglist capitalize")
 
-            strRW += "</td><td>" + director.get_text() + "</td><td>" + length.get_text().lstrip()[0:8].rstrip() + "</td><td>" + genre.get_text().strip().replace(' ', ', ') + "</td></tr>\n"
-            file.write(strRW)
+            director = soup.find_all('span', class_ = "prettify")
+            if director is not None and len(director) != 0:
+                director = [name.get_text() for name in director]
+                director = ", ".join(director).strip()
+            else:
+                director = "N/A"
+            
+            length = soup.find('p', class_ = "text-link text-footer").get_text().lstrip()[0:8].rstrip()
+            if not length[0].isnumeric():
+                length = "N/A"
+
+            genre = soup.find('div', class_ = "text-sluglist capitalize")
+            if genre is not None:
+                genre = genre.get_text().strip().replace(' ', ', ')
+            else:
+                genre = "N/A"
+
+            tableRow += "</td><td>" + director + "</td><td>" + length + "</td><td>" + genre + "</td></tr>\n"
+            file.write(tableRow)
 
         file.write('''</table>
         </body>
